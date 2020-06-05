26b92d4e876bc8775bd584700d92b8ed80b78853
Valdis <valpivars@gmail.com>
2020-06-05 16:45:03 +0300

    v1

---
+++ Game.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeGame
+{
+    public partial class Game : Form
+    {
+        GameZone gameZone = null;
+        Snake snake = null;
+        public Game()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            this.Width = 500;
+            this.Height = 550;
+
+            gameZone = new GameZone();
+            gameZone.Location = new Point(50, 50);
+            this.Controls.Add(gameZone);
+
+            snake = new Snake();
+
+
+        }
+    }
+}
---
+++ Snake.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeGame
+{
+    class Snake
+    {
+        private Form game = null;
+        PictureBox snakePixel;
+        List<PictureBox> snakePixels = new List<PictureBox>();
+
+        public Snake(Form form)
+        {
+            InitializeSnake();
+        }
+        private void InitializeSnake()
+        {
+            snakePixel = new PictureBox()
+            {
+                BackColor = Color.Green,
+                Width = 20,
+                Height = 20,
+                Left = 190,
+                Top = 190
+            };
+            snakePixels.Add(snakePixel);
+        }
+
+        public void Render()
+        {
+            game.Controls.Add(snakePixels[0]);
+        }
+
+    }
+}
